bc6f6359cd0a15ab211d5e2f51cd7f3a5a2da65f
panchepanevski <pance.panevski@gmail.com>
2020-02-05 09:28:00 +0100

    Add styling and animation

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// ANIMATION ON SCROLL
 AOS.init();
+// END ANIMATION ON SCROLL
+
+// BURGER MENU
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav__links");
+  const navLinks = document.querySelectorAll(".nav__links li");
+  // Toggle Nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          0.5}s`;
+      }
+    });
+    // Burger Animate
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
